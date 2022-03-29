e9d9c4a8f9b4219bdc4878e312056b7e7778d8b8
eexpress <eexpress@163.com>
2022-03-29 13:12:26 +0800

    prefs 42 only

--- prefs.js
+++ prefs.js
@@ -11,17 +11,12 @@ function init() {
 	ExtensionUtils.initTranslations();
 }
 
-function buildPrefsWidget() {
-	return new MyPrefs();
-}
-
 function fillPreferencesWindow(window) {
 	let page = Adw.PreferencesPage.new();
 	page.add(new MyPrefs());
 	window.set_default_size(500, 200);
 	window.add(page);
 }
-//~ https://gjs.guide/extensions/upgrading/gnome-shell-42.html#fillpreferenceswindow
 
 class MyPrefs extends Adw.PreferencesGroup {
 	static {
@@ -29,29 +24,26 @@ class MyPrefs extends Adw.PreferencesGroup {
 	}
 	constructor() {
 		super();
-		this.settings = ExtensionUtils.getSettings();
-		log(this.settings.get_string('longitude'));
-		log(this.settings.get_string('latitude'));
-		//~ Romain: While your window is opened, You can use the GTK inspector to interactively try properties changes on your widgets, press Ctrl+Shift+D, then you'll be able to pick your widgets and change the properties.
 		this._longitude = new Gtk.Entry({ valign : Gtk.Align.CENTER });
 		this._latitude = new Gtk.Entry({ valign : Gtk.Align.CENTER });
-		this.add(new PrefRow(this._longitude, _('longitude'), _('longitude of your city')));
-		this.add(new PrefRow(this._latitude, _('latitude'), _('latitude of your city')));
-		this.settings.bind('longitude', this._longitude, 'text', Gio.SettingsBindFlags.DEFAULT);
-		this.settings.bind('latitude', this._latitude, 'text', Gio.SettingsBindFlags.DEFAULT);
+		[
+			[ this._longitude, _('longitude'), _('longitude of your city'), 'longitude' ],
+			[ this._latitude, _('latitude'), _('latitude of your city'), 'latitude' ]
+		].forEach(e => this.add(new MyRow(...e)));
 	}
 }
 
-class PrefRow extends Adw.ActionRow {
+class MyRow extends Adw.ActionRow {
 	static {
 		GObject.registerClass(this);
 	}
-	constructor(...args) {
+	constructor(suffix_widget, title, subtitle, schemas_key) {
 		super();
-		let [prefix, title, subtitle] = args;
 		this.set_title(title);
 		this.set_subtitle(subtitle);
-		this.add_suffix(prefix);
-		this.set_activatable_widget(prefix);
+		this.add_suffix(suffix_widget);
+		this.set_activatable_widget(suffix_widget);
+		this.settings = ExtensionUtils.getSettings();
+		this.settings.bind(schemas_key, suffix_widget, 'text', Gio.SettingsBindFlags.DEFAULT);
 	}
 }
