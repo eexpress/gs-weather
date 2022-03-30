39c87abf30e8aafb76466537fc54d515b98c40f7
eexpress <eexpress@163.com>
2022-03-30 13:21:26 +0800

    get_web不能跨类

--- prefs.js
+++ prefs.js
@@ -30,20 +30,24 @@ class MyPrefs extends Adw.PreferencesGroup {
 		this._longitude = new Gtk.Entry();
 		this._latitude = new Gtk.Entry();
 		[
-			[ this._longitude, _('longitude'), _('longitude of your city'), 'longitude' ],
-			[ this._latitude, _('latitude'), _('latitude of your city'), 'latitude' ]
-		].forEach(e => this.add(new MyRow(...e)));
+			[ this._longitude, true, _('longitude'), _('longitude of your city'), 'longitude' ],
+			[ this._latitude, true, _('latitude'), _('latitude of your city'), 'latitude' ]]
+			.forEach(e => this.add(new MyRow(...e)));
 
-		this._input = new Gtk.Entry();
-		this.add(new MyRow(this._input, _('City'), _('search your city'), ''));
+		this._input = new Gtk.Entry({
+			secondary_icon_name : 'folder-saved-search-symbolic',
+		});
+		this.add(new MyRow(this._input, true, _('City'), _('search your city'), ''));
 		this._input.connect('activate', this.get_web.bind(this));
 	}
 
 	get_web() {
+		const city = this._input.text;
+		if (city == '' || city.length < 3) return;
 		let params = {
 			appid : 'c93b4a667c8c9d1d1eb941621f899bb8',
 			limit : '3',
-			q : this._input.text
+			q : city
 		};
 		let url = 'https://api.openweathermap.org/geo/1.0/direct';
 		try {
@@ -53,26 +57,26 @@ class MyPrefs extends Adw.PreferencesGroup {
 			session.queue_message(message, () => {
 				const response = message.response_body.data;
 				const obj = JSON.parse(response);
-				log("get_web OK.");
-				list.forEach(k => {if(k){ this.remove(k);}});
+				list.forEach(k => {if(k){ this.remove(k);} });
 				list = [];
 				let cbg;
-				if(obj[0] != null){	// 有数据
-					for(let i in obj){
+				if (obj[0] != null) {  // 有数据
+					for (let i in obj) {
 						const k = obj[i];
 						const cb = new Gtk.CheckButton();
 						cb.connect('activate', () => {
-							log(k.lon+', '+k.lat);
+							log(k.lon + ', ' + k.lat);
 							this._longitude.text = k.lon.toString();
 							this._latitude.text = k.lat.toString();
 						});
-						if(i == 0) cbg = cb; else cb.set_group(cbg);
+						if (i == 0) cbg = cb;
+						else cb.set_group(cbg);
 						let zh = '';
-						log(i);
-						if(k.local_names && k.local_names.zh != undefined) zh = k.local_names.zh;
+						if (k.local_names && k.local_names.zh != undefined) zh = k.local_names.zh;
 						list[i] = new MyRow(cb,
-							[k.name, k.state, k.country, zh].join(', '),
-							[k.lon, k.lat].join(', '),
+							false,
+							[ k.name, k.state, k.country, zh ].join(', '),
+							[ k.lon, k.lat ].join(', '),
 							'');
 						this.add(list[i]);
 					}
@@ -80,18 +84,26 @@ class MyPrefs extends Adw.PreferencesGroup {
 			});
 		} catch (e) { throw e; }
 	}
-
 }
 
 class MyRow extends Adw.ActionRow {
 	static {
 		GObject.registerClass(this);
 	}
-	constructor(suffix_widget, title, subtitle, schemas_key) {
+	constructor(suffix_widget, isEntry, title, subtitle, schemas_key) {
 		super();
 		this.set_title(title);
 		this.set_subtitle(subtitle);
 		suffix_widget.valign = Gtk.Align.CENTER;
+		if (isEntry) {
+			suffix_widget.primary_icon_name = 'edit-clear-all-symbolic';
+			suffix_widget.connect('icon-press', (obj, pos, event) => {
+				if (pos == Gtk.EntryIconPosition.PRIMARY) obj.text = '';
+				else {
+					if (obj.secondary_icon_activatable) suffix_widget.super.get_web();
+				}
+			});
+		}
 		this.add_suffix(suffix_widget);
 		this.set_activatable_widget(suffix_widget);
 		if (schemas_key) {
