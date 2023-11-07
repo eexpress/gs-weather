bce078c45b8931795d79745c0fbd76fc3a316069
eexpress <eexpress@163.com>
2023-11-07 21:11:47 +0800

    45

---
+++ pack.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#~ coord.json废弃了
+
+gnome-extensions pack -f --extra-source=weather-icon
+
+gnome-extensions install git@eexpss.gmail.com.shell-extension.zip -f
+
+#~ ⭕ pi gettext
+#~ msgfmt
